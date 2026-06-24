1d06dfd95b6c85e7b26205d6fc5f86d0c6e25f38
flbezz <bezawoldiye29@gmail.com>
2026-06-24 05:01:01 -0700

    basketball scoreboard core files

---
+++ basket.js
@@ -0,0 +1,28 @@
+let home= document.getElementById("dashboard");
+let guest=document.getElementById("dashboardb");
+let a=12;
+let b=5;
+function oneh(){
+a++;
+home.innerText=a;
+}
+function twoh(){
+a=a+2;
+home.innerText=a;
+}
+function threeh(){
+a=a+3;
+home.innerText=a;
+}
+function oneg(){
+b++;
+guest.innerText=b;
+}
+function twog(){
+b=b+2;
+guest.innerText=b;
+}
+function threeg(){
+b=b+3;
+guest.innerText=b;
+}
